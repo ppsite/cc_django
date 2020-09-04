7c9a4748ab6be983451cf6855321fce5f7b0ccae
figure <peizhenfei@cvte.com>
2020-09-04 15:28:39 +0800

    1. pyenv 自动创建 virtualenv 支持 2. 更新 README 文档

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,9 +1,14 @@
 import re
 import sys
+from subprocess import check_call
 
 
 class PreGenProjectHooks(object):
     """生成项目前的钩子"""
+    ENV_COMMANDS = [
+        'pyenv virtualenv 3.7.5 {{cookiecutter.project_name}}',
+        'pyenv local {{cookiecutter.project_name}}'
+    ]
 
     @staticmethod
     def check_project_name():
@@ -13,7 +18,12 @@ class PreGenProjectHooks(object):
             print('ERROR: The project name(%s) invalid Python module name. use _ instead of -' % module_name)
             sys.exit(1)
 
+    def pyenv_virtualenv(self):
+        for cmd in self.ENV_COMMANDS:
+            check_call(cmd.split())
+
 
 if __name__ == '__main__':
     hooks = PreGenProjectHooks()
     hooks.check_project_name()
+    hooks.pyenv_virtualenv()
